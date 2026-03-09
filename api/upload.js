88fc185315633a00d1a7fde4b5de6a66f9a3829f
cauvangADMIN <cauvangmemeshop@gmail.com>
2026-03-09 17:10:49 +0700

    Connect to R2

---
+++ api/upload.js
@@ -0,0 +1,42 @@
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3"
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
+
+const client = new S3Client({
+  region: "auto",
+  endpoint: process.env.R2_ENDPOINT,
+  credentials: {
+    accessKeyId: process.env.R2_ACCESS_KEY,
+    secretAccessKey: process.env.R2_SECRET_KEY,
+  },
+})
+
+export default async function handler(req, res) {
+
+  const chunks = []
+
+  for await (const chunk of req)
+    chunks.push(chunk)
+
+  const buffer = Buffer.concat(chunks)
+
+  const filename = Date.now() + ".jpg"
+
+  await client.send(
+    new PutObjectCommand({
+      Bucket: process.env.R2_BUCKET,
+      Key: filename,
+      Body: buffer,
+      ContentType: "image/jpeg"
+    })
+  )
+
+  const url =
+    `https://pub-da217e9da43e4b3ba192844eb749f8ad.r2.dev/${filename}`
+
+  res.json({ url })
+}
