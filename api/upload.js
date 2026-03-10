c2f1fb0436566b1411b8612fd62c97a187b0f5bb
cauvangADMIN <cauvangmemeshop@gmail.com>
2026-03-10 08:58:54 +0700

    Fix bug R2

--- api/upload.js
+++ api/upload.js
@@ -1,19 +1,19 @@
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3"
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-}
-
 const client = new S3Client({
   region: "auto",
   endpoint: process.env.R2_ENDPOINT,
   credentials: {
     accessKeyId: process.env.R2_ACCESS_KEY,
-    secretAccessKey: process.env.R2_SECRET_KEY,
-  },
+    secretAccessKey: process.env.R2_SECRET_KEY
+  }
 })
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+}
 
 export default async function handler(req, res) {
 
@@ -26,17 +26,15 @@ export default async function handler(req, res) {
 
   const filename = Date.now() + ".jpg"
 
-  await client.send(
-    new PutObjectCommand({
-      Bucket: process.env.R2_BUCKET,
-      Key: filename,
-      Body: buffer,
-      ContentType: "image/jpeg"
-    })
-  )
+  await client.send(new PutObjectCommand({
+    Bucket: process.env.R2_BUCKET,
+    Key: filename,
+    Body: buffer,
+    ContentType: "image/jpeg"
+  }))
 
   const url =
     `https://pub-da217e9da43e4b3ba192844eb749f8ad.r2.dev/${filename}`
 
-  res.json({ url })
+  res.status(200).json({ url })
 }
