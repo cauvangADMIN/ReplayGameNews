fa5a17b2b65e36410e314da1bf6b14c331e906ab
cauvangADMIN <cauvangmemeshop@gmail.com>
2026-03-18 21:54:24 +0700

    Update for - Push Notification - Monetag

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 10746943
+    "zoneId": 10747017
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
